56435653a7b100486c8e6935dce82530acbc3077
Roman <roman.cherniy89@gmail.com>
2017-05-08 18:05:05 +0300

    2

--- script.js
+++ script.js
@@ -39,14 +39,14 @@ Magnifier.prototype.variables = function () {
   this.ratioWidth               = this.magnifierImgWidth/this.blockImgWidth;
   this.widthMagnifer = parseInt(getComputedStyle(this.blockMagnifier).width) / 2;
   this.heightMagnifer = parseInt(getComputedStyle(this.blockMagnifier).height) / 2;
-  this.blockMagnifierTop            = parseInt(getComputedStyle(this.blockMagnifier).top);
-  this.blockMagnifierLeft            = parseInt(getComputedStyle(this.blockMagnifier).left);
+  this.top            = parseInt(getComputedStyle(this.blockMagnifier).top);
+  this.left            = parseInt(getComputedStyle(this.blockMagnifier).left);
 }
 
 Magnifier.prototype.AddEventMouse = function (event) {
-  this.xStart             = event.offsetX;
-  this.yStart               = event.offsetY;
-  this.blockMagnifier.style.cssText +="display:block;";
+  this.xStart = event.offsetX;
+  this.yStart = event.offsetY;
+  this.blockMagnifier.style.cssText +="display:block;top: "+this.yStart+"px;left: "+this.xStart+"px;display:block;";
   this.blockMagnifier.addEventListener("mousemove", this.AddEventMouseMove.bind(this));
 }
 
@@ -65,18 +65,29 @@ Magnifier.prototype.AddEventMouseMove = function (event) {
   this.blockMagnifierLeft            = parseInt(getComputedStyle(this.blockMagnifier).left);
 
 
-      if(this.moveMouseY > this.heightMagnifer) {
-        resultPositionTop = this.blockMagnifierTop + (this.moveMouseY-this.heightMagnifer);
-      } else if (this.moveMouseY < this.heightMagnifer) {
-        resultPositionTop = this.blockMagnifierTop + (this.moveMouseY-this.heightMagnifer);
-      }
+    var   resultPositionTop = this.blockMagnifierTop + (this.moveMouseY-this.heightMagnifer),
+          resultPositionLeft = this.blockMagnifierLeft + (this.moveMouseX-this.widthMagnifer);
 
-      if(this.moveMouseX > this.widthMagnifer) {
-        resultPositionLeft = this.blockMagnifierLeft + (this.moveMouseX-this.widthMagnifer);
-      }else if (this.moveMouseX < this.widthMagnifer) {
-        resultPositionLeft = this.blockMagnifierLeft + (this.moveMouseX-this.widthMagnifer);
-      }
+
 
   //this.magnifierImg.style.cssText +="transform: translate(-"+(this.moveMouseX * this.ratioWidth)+"px,-"+(this.moveMouseY * this.ratioHeight)+"px);";
     this.blockMagnifier.style.cssText ="top: "+resultPositionTop+"px;left: "+resultPositionLeft+"px;display:block;";
+    this.hideMagnifier();
+}
+
+Magnifier.prototype.hideMagnifier = function () {
+  console.log(this.top);
+  console.log(this.blockMagnifierTop);
+
+
+  if (this.blockMagnifierTop >= this.top && this.blockMagnifierTop > this.blockImgHeight) {
+    alert('123');
+    this.blockMagnifier.style.cssText +="display:none;top:0;left:0;";
+  }
+
+  if (this.blockMagnifierLeft >= this.left && this.blockMagnifierLeft > this.blockImgWidth) {
+    this.blockMagnifier.style.cssText +="display:none;top:0;left:0;";
+  }
+
+
 }
